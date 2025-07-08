8e9019b61e1a2c0b518c1092e765d47fc8a68a42
LakshanWC <lakshanwc@gmail.com>
2025-07-08 19:23:03 +0530

    added QR Code scanner

--- TuitionManagerApp/app/build.gradle.kts
+++ TuitionManagerApp/app/build.gradle.kts
@@ -48,4 +48,5 @@ dependencies {
     implementation(platform("com.google.firebase:firebase-bom:33.16.0"))
     implementation("com.google.firebase:firebase-analytics")
     implementation("com.google.android.material:material:1.11.0")
+    implementation("com.journeyapps:zxing-android-embedded:4.3.0")  //for qr code scaner
 }
