2211ce7ae2eab584caf37334d48a2cd901de3de6
LakshanWC <lakshanwc@gmail.com>
2025-07-08 13:11:20 +0530

    update files
    update teacher home
    chagned google-services(1).json to google-services.json

--- TuitionManagerApp/app/src/main/java/com/wc/tuitionmanagerapp/teacher/TeacherHome.java
+++ TuitionManagerApp/app/src/main/java/com/wc/tuitionmanagerapp/teacher/TeacherHome.java
@@ -1,6 +1,8 @@
 package com.wc.tuitionmanagerapp.teacher;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -22,5 +24,22 @@ public class TeacherHome extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    public void goToAssignmentUi(View view){
+        Intent assingmentIntent = new Intent(TeacherHome.this,Assignment.class);
+        startActivity(assingmentIntent);
+    }
+    public void goToCourseMaterialUi(View view){
+        Intent courseMaterialIntent = new Intent(TeacherHome.this,CourseMaterial.class);
+        startActivity(courseMaterialIntent);
+    }
+    public void goToAttendenceUi(View view){
+        Intent attendenceIntent = new Intent(TeacherHome.this,Attendence.class);
+        startActivity(attendenceIntent);
+    }
+    public void goToReultUi(View view){
+        Intent resultIntent = new Intent(TeacherHome.this,Result.class);
+        startActivity(resultIntent);
     }
 }
