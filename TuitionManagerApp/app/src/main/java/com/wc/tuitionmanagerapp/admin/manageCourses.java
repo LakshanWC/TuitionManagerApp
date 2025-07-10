ee11b85fb021cd1a6dc4821853176850243925db
Wasath Deminda <jayawardanawasath@gmail.com>
2025-07-10 17:31:56 +0530

    change the ui of the admin related menus

--- TuitionManagerApp/app/src/main/java/com/wc/tuitionmanagerapp/admin/manageCourses.java
+++ TuitionManagerApp/app/src/main/java/com/wc/tuitionmanagerapp/admin/manageCourses.java
@@ -2,6 +2,8 @@ package com.wc.tuitionmanagerapp.admin;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
 import androidx.appcompat.app.AppCompatActivity;
 import com.wc.tuitionmanagerapp.R;
 
@@ -13,24 +15,44 @@ public class manageCourses extends AppCompatActivity {
         setContentView(R.layout.activity_manage_courses);
 
 
-       findViewById(R.id.btnAddCourse).setOnClickListener(v -> {
-           Intent intent = new Intent(manageCourses.this, addCourse.class);
-           startActivity(intent);
-        });
+//       findViewById(R.id.btnAddCourse).setOnClickListener(v -> {
+//           Intent intent = new Intent(manageCourses.this, addCourse.class);
+//           startActivity(intent);
+//        });
+//
+//        findViewById(R.id.btnRemoveCourse).setOnClickListener(v -> {
+//            Intent intent = new Intent(manageCourses.this, removeCourse.class);
+//            startActivity(intent);
+//        });
+//
+//        findViewById(R.id.btnAssignTeachers).setOnClickListener(v -> {
+//            Intent intent = new Intent(manageCourses.this, assignTeacher.class);
+//            startActivity(intent);
+//        });
+//
+//        findViewById(R.id.btnAssignStudents).setOnClickListener(v -> {
+//            Intent intent = new Intent(manageCourses.this, assignStudent.class);
+//            startActivity(intent);
+//        });
+    }
+    // Add these methods to your existing manageCourses class
+    public void goToAddCourse(View view) {
+        Intent intent = new Intent(manageCourses.this, addCourse.class);
+        startActivity(intent);
+    }
 
-        findViewById(R.id.btnRemoveCourse).setOnClickListener(v -> {
-            Intent intent = new Intent(manageCourses.this, removeCourse.class);
-            startActivity(intent);
-        });
+    public void goToRemoveCourse(View view) {
+        Intent intent = new Intent(manageCourses.this, removeCourse.class);
+        startActivity(intent);
+    }
 
-        findViewById(R.id.btnAssignTeachers).setOnClickListener(v -> {
-            Intent intent = new Intent(manageCourses.this, assignTeacher.class);
-            startActivity(intent);
-        });
+    public void goToAssignTeachers(View view) {
+        Intent intent = new Intent(manageCourses.this, assignTeacher.class);
+        startActivity(intent);
+    }
 
-        findViewById(R.id.btnAssignStudents).setOnClickListener(v -> {
-            Intent intent = new Intent(manageCourses.this, assignStudent.class);
-            startActivity(intent);
-        });
+    public void goToAssignStudents(View view) {
+        Intent intent = new Intent(manageCourses.this, assignStudent.class);
+        startActivity(intent);
     }
 }
