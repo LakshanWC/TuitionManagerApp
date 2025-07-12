837a27dd114d4cba7a301d5d563ba3d556ec13e6
LakshanWC <lakshanwc@gmail.com>
2025-07-12 10:53:39 +0530

    added qr generation to student

--- TuitionManagerApp/app/src/main/java/com/wc/tuitionmanagerapp/teacher/Attendence.java
+++ TuitionManagerApp/app/src/main/java/com/wc/tuitionmanagerapp/teacher/Attendence.java
@@ -1,10 +1,13 @@
 package com.wc.tuitionmanagerapp.teacher;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,7 +16,11 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 import com.wc.tuitionmanagerapp.R;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class Attendence extends AppCompatActivity {
 
@@ -43,4 +50,48 @@ public class Attendence extends AppCompatActivity {
     public void goesHome(View view) {
         NavigateUtil.goToTeacherHome(this);
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if (result != null) {
+            if (result.getContents() != null) {
+                // Example: scannedData = "Lakshan:U101"
+                String scannedData = result.getContents();
+                handleScannedData(scannedData);
+            } else {
+                Toast.makeText(this, "Scan cancelled", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private void handleScannedData(String data) {
+        // Example data format: "Lakshan:U101"
+        String[] parts = data.split(":");
+        if (parts.length == 2) {
+            String studentName = parts[0];
+            String studentId = parts[1];
+
+            //get the date and time
+            LocalDateTime now = LocalDateTime.now();
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+            String formattedDateTime = now.format(formatter);
+
+            // Assign to UI
+            TextView txtStudentName = findViewById(R.id.tvStudentName);
+            TextView txtStudentId = findViewById(R.id.tvStudentId);
+            TextView txtDateTime = findViewById(R.id.tvDateTime);
+
+
+            txtStudentName.setText(studentName);
+            txtStudentId.setText(studentId);
+            txtDateTime.setText(formattedDateTime);
+        } else {
+            Toast.makeText(this, "Invalid QR format", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
 }
